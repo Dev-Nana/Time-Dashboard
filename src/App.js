dd30e684bc273d68324d609d952cdf3ea6f2b9a2
Nana <evylaveras@gmail.com>
2024-09-19 10:40:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="app_content">
       <UserCard/>
     </div>
   );
